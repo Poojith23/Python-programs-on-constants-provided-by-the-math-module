f7e6cb43035afb114ac6ebef6a731a14964c34d7
Poojith23 <114901320+Poojith23@users.noreply.github.com>
2022-12-23 23:04:00 +0530

    Add files via upload

---
+++ Python programs on constants provided by the math module.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Import math Library
+import math
+ 
+# Print the value of Euler e
+print (math.e)
+
+
+# In[2]:
+
+
+# Import math Library
+import math
+ 
+# Print the value of pi
+print (math.pi)
+
+
+# In[3]:
+
+
+# Import math Library
+import math
+ 
+# radius of the circle
+r = 4
+ 
+# value of pie
+pie = math.pi
+ 
+# area of the circle
+print(pie * r * r)
+
+
+# In[4]:
+
+
+# Import math Library
+import math
+ 
+# Print the value of tau
+print (math.tau)
+
+
+# In[5]:
+
+
+# Import math Library
+import math
+ 
+# Print the positive infinity
+print (math.inf)
+ 
+# Print the negative infinity
+print (-math.inf)
+
+
+# In[7]:
+
+
+#Comparing the values of infinity with the maximum floating point value
+
+# Import math Library
+import math
+ 
+print (math.inf > 10e108)
+print (-math.inf < -10e108)
+
+
+# In[ ]:
+
+
+
+
